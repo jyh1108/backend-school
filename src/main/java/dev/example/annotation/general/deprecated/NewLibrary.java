e66f2f233ee4fe855144546c40967b5c782042bf
jyong yong hwa <kobcom12@naver.com>
2025-05-27 17:45:33 +0900

    [Annotation, Reflection] General Annotion: Deprecated

---
+++ src/main/java/dev/example/annotation/general/deprecated/NewLibrary.java
@@ -0,0 +1,7 @@
+package dev.example.annotation.general.deprecated;
+
+public class NewLibrary {
+    public void printMessage(){
+        System.out.println("I'm new library");
+    }
+}
---
+++ src/main/java/dev/example/annotation/general/deprecated/OldLibrary.java
@@ -0,0 +1,8 @@
+package dev.example.annotation.general.deprecated;
+
+public class OldLibrary {
+    @Deprecated
+    public void printMessage(){
+        System.out.println("I'm old library");
+    }
+}
